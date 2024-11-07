abcbb7006d5476665b7bf14cab1cb6ff1d817594
red5191 <the_red51@outlook.com>
2024-11-07 16:16:10 +0300

    pepeW

--- main.py
+++ main.py
@@ -2,20 +2,26 @@ class BankAccount:
     def __init__(self):
         self.__balance = 0
 
-    def deposite(self, add):
-        self.__balance += add
-        print(f'Счет пополнен на {add} MNT')
+    def set_balance(self, deposite, withdraw):
+        try:
+            if deposite > 0 and withdraw > 0:
+                self.__balance += float(deposite)
+                print(f'Счет пополнен на {deposite} MNT')
 
-    def withdraw(self, remove):
-        self.__balance -= remove
-        print(f'Со счета списано {remove} MNT')
+                self.__balance -= float(withdraw)
+                print(f'Со счета было списано {withdraw} MNT')
+            else:
+                print('Депозит и списание должны быть положительными числами')
+        except ValueError:
+            print('Депозит и списание должны быть положительными числами')
 
-    def check_balance(self):
+    def get_balance(self):
         print(f'Баланс по счету составляет {self.__balance} MNT')
+
+    balance = property(get_balance, set_balance)
 
 
 myacc = BankAccount()
 
-myacc.deposite(15000)
-myacc.withdraw(4567)
-myacc.check_balance()
+myacc.set_balance(15000, 4567)
+myacc.get_balance()
